6e91e70f723d5c1cf4497d0b4e207c94d3465458
maben <mabenchn@gmail.com>
2019-01-23 21:18:19 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">
+			design by：<a href="#" target="_blank">mabenchn@gmail.com</a>
+		</p>
+
+		<p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+	</div>
+</footer>
