ea9941f7e04727ad1210d6f58ddd1864cc9420c5
maben <mabenchn@gmail.com>
2019-01-26 12:43:37 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,7 @@ class TopicObserver
     //在topic模型保存时候触发saving事件，对excerpt字段进行赋值
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
     	$topic->excerpt = make_excerpt($topic->body);
     }
 }
