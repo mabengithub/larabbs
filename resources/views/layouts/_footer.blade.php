cb231db31450841fd64ee31ff5b8edf68335047f
maben <mabenchn@gmail.com>
2019-01-27 21:42:26 +0800

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
 			design by：<a href="#" target="_blank">mabenchn@gmail.com</a>
 		</p>
 
-		<p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+		<p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
 	</div>
 </footer>
